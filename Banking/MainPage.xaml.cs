6578293b58a05ac1d2df66fe37b2db347d772f7c
Kees Hiemstra <chi@xs4all.nl>
2019-02-11 09:31:19 +0100

    Added basis navigation

--- Banking/MainPage.xaml.cs
+++ Banking/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking.Pages;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,14 +19,37 @@ using MUXC = Microsoft.UI.Xaml.Controls;
 
 namespace Banking
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
+      this.InitializeComponent();
     }
+
+    private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+      if (args.IsSettingsSelected)
+      {
+        ContentFrame.Navigate(typeof(SettingsPage));
+        NavView.Header = "Settings";
+      }
+      else
+      {
+        NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+
+        switch (item.Tag.ToString())
+        {
+          case "home":
+            ContentFrame.Navigate(typeof(HomePage));
+            NavView.Header = "Home";
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
 }
