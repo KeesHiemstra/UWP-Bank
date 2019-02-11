6d11ca3da099cf79f6c7217a16749239c3afddc3
Kees Hiemstra <chi@xs4all.nl>
2019-02-11 15:10:46 +0100

    Added models classes

---
+++ Banking/Models/ABNImport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Models
+{
+	class ABNImport
+	{
+		public DateTime Date { get; set; }
+		public string Name { get; set; }
+		public string Account { get; set; }
+		public string CounterAccount { get; set; }
+		public decimal Amount { get; set; }
+		public string Mutation { get; set; }
+		public string Text { get; set; }
+	}
+}
