53b2693e6792fe2dfa23b9539ef62479a548839a
Kees Hiemstra <chi@xs4all.nl>
2019-02-13 10:25:12 +0100

    Get EntityFramework working

--- Banking/Views/ImportView.cs
+++ Banking/Views/ImportView.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Banking.Models;
+using Microsoft.SqlServer;
 
 namespace Banking.Views
 {
   public class ImportOverview
   {
+    private static List<ImportCount> _ImportCounts;
 
+    public ImportOverview(List<ImportCount> ImportCounts)
+    {
+      _ImportCounts = ImportCounts;
+
+      
+    }
   }
 }
